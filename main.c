d17c481c9633fd2c67526e1fff64cb34410114dc
Malak47 <malak.qeedan@hotmail.com>
2022-11-29 21:34:42 +0200

    updating..

--- main.c
+++ main.c
@@ -6,37 +6,38 @@
 
 int main() {
     int matrix[SIZE][SIZE];
-    char ch;
+    char ch = ' ';
 
     while (ch != 'D') {
         scanf("%s", &ch);
+        int i, j;
         switch (ch) {
 
-            case 'A':;
-                for (int i = 0; i < SIZE; ++i) {
-                    for (int j = 0; j < SIZE; ++j) {
+            case 'A':
+                for (i = 0; i < SIZE; ++i) {
+                    for (j = 0; j < SIZE; ++j) {
                         scanf("%d", &matrix[i][j]);
                     }
                 }
                 A(matrix);
                 break;
 
-            case 'B':;
-                int i, j;
+            case 'B':
                 scanf("%d%d", &i, &j);
                 B(i, j);
                 break;
 
-            case 'C':;
-                int k, l;
-                scanf("%d%d", &k, &l);
-                C(k, l);
+            case 'C':
+                scanf("%d%d", &i, &j);
+                C(i, j);
                 break;
 
-            case 'D':;
+            case 'D':
                 D();
                 break;
 
+            default:
+                printf("Error: invalid input, Enter 'D' for exit.\n");
         }
     }
     return True;
