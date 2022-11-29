add27555083157eb36d8e07836081fbefcfdf2c8
Malak47 <malak.qeedan@hotmail.com>
2022-11-29 20:17:44 +0200

    Creating header file

---
+++ my_mat.h
@@ -0,0 +1,21 @@
+//
+// Created by malak on 11/29/2022.
+//
+
+#ifndef S_S_A_2_MY_MAT_H
+#define S_S_A_2_MY_MAT_H
+
+#define True 1
+
+#define SIZE 10
+#define MAXVALUE INT_MAX
+
+void A(int arr[SIZE][SIZE]);
+
+void B(int i, int j);
+
+void C(int i, int j);
+
+void D();
+
+#endif //S_S_A_2_MY_MAT_H
